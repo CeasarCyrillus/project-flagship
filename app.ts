86d9c6f94590971e4cea8b8b95001425b2a164b2
Ceasar Cyrillus <ceasarcyrillus@businesspartner.roche.com>
2020-05-16 15:46:39 +0200

    HELLO DENO

---
+++ app.ts
@@ -0,0 +1,21 @@
+import { Drash } from "https://deno.land/x/drash@v1.0.0/mod.ts";
+
+class HomeResource extends Drash.Http.Resource {
+  static paths = ["/"];
+  public GET() {
+    this.response.body = "Hello World! deno + Drash is cool!";
+    return this.response;
+  }
+}
+
+const server = new Drash.Http.Server({
+  response_output: "text/html",
+  resources: [HomeResource]
+});
+
+server.run({
+  hostname: "localhost",
+  port: 1447
+});
+
+console.log("Server listening: http://localhost:1447");
