04c441bd2b2750c09a77b24a80d36a10bc915021
Ceasar Cyrillus <ceasarcyrillus@businesspartner.roche.com>
2020-05-16 16:32:40 +0200

    Clear domain object from untested properties

--- domain/lobby.ts
+++ domain/lobby.ts
@@ -1,24 +1,14 @@
 import Player from "./player.ts";
-import InvalidLobbyCodeError from "./exceptions/invalidLobbyCode.ts";
 import LobbyCode from "./lobbyCode.ts";
 
 class Lobby {
     public code: LobbyCode;
     public description: string;
-    private players: Player[] = new Array<Player>();
 
     constructor(code?: string, description?: string) {
         this.code = new LobbyCode(code);
         this.description = description ? description : `Lobby for game #${this.code}`;
     }
-
-    public addPlayer(player: Player): void {
-        const playerUsernameExists = this.getPlayers().findIndex(p => player.username === p.username) !== -1;
-        if(playerUsernameExists) throw new Error("Username isn't unique");
-        this.players.push(player);
-    }
-
-    public getPlayers = () => [...this.players];
 }
 
 export default Lobby;
--- test/domain/lobby.test.ts
+++ test/domain/lobby.test.ts
@@ -1,9 +1,6 @@
 import Lobby from "../../domain/lobby.ts";
-import InvalidLobbyCodeError from "../../domain/exceptions/invalidLobbyCode.ts";
-import { assertNotEquals, assertThrows, assertStrContains } from "https://deno.land/std/testing/asserts.ts"
+import { assertNotEquals, assertStrContains } from "https://deno.land/std/testing/asserts.ts"
 
-
-//#region Lobby
 Deno.test("Lobby has default lobby code", () => {
     const lobby = new Lobby();
     
@@ -16,5 +13,3 @@ Deno.test("Lobby description will contain code", () => {
     const lobby = new Lobby();
     assertStrContains(lobby.description, lobby.code.toString());
 });
-
-//#endregion
